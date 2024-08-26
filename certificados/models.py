16004747709949cdf1d20af84967ff76e5b2b5c6
Lucas Soares <soareslukas9090@hotmail.com>
2024-08-26 13:14:22 -0300

    feat: configuração do banco de dados e variáveis de ambiente

--- certificados/models.py
+++ certificados/models.py
@@ -1,3 +1,83 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    Permission,
+    PermissionsMixin,
+)
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(
+        self,
+        name,
+        email,
+        password=None,
+        **extra_fields,
+    ):
+        if not email:
+            raise ValueError("User must have an email address")
+        if not name:
+            raise ValueError("User must have a name")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+            **extra_fields,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(
+        self,
+        name,
+        email,
+        password=None,
+        **extra_fields,
+    ):
+        user = self.create_user(
+            name=name,
+            email=self.normalize_email(email),
+            password=password,
+            is_admin=True,
+            is_superuser=True,
+            **extra_fields,
+        )
+
+        permissions = Permission.objects.all()
+        user.user_permissions.set(permissions)
+
+        return user
+
+
+class Users(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=255, null=False)
+    cnpj = models.CharField(max_length=14, null=False, unique=True)
+    email = models.EmailField(null=False, unique=True)
+    password = models.CharField(max_length=512, null=False)
+    is_admin = models.BooleanField(null=False, default=False)
+    is_active = models.BooleanField(null=False, default=True)
+
+    REQUIRED_FIELDS = [
+        "name",
+        "cnpj",
+    ]
+
+    USERNAME_FIELD = "email"
+
+    objects = UserManager()
+
+    def __str__(self):
+        str = f"{self.email}"
+        return str
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower()
+
+        super().save(*args, **kwargs)
